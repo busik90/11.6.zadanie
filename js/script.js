f677a16d3111774f28fff46040fe504f33c04e69
Pawel Bus <pawelbus.dev@outlook.com>
2018-05-10 11:14:07 +0200

    Make object board

--- js/script.js
+++ js/script.js
@@ -8,81 +8,90 @@ $( function() {
         str += chars[Math.floor(Math.random() * chars.length)];
     }
     return str;
-}
-
-function Column(name) {
-  var self = this;
-
-  this.id = randomString();
-  this.name = name;
-  this.$element = createColumn();
-
-  function createColumn() {
-    // create components of columns
-    var $column = $('<div>').addClass('column'),
-        $columnTitle = $('<h2>').addClass('column-title').text(self.name),
-        $columnCardList = $('<ul>').addClass('column-card-list'),
-        $columnDelete = $('<button>').addClass('btn-delete').text('x'),
-        $columnAddCard = $('<button>').addClass('add-card').text('Add a card');
-
-    // events
-    $columnDelete.click(function() {
-      self.removeColumn();
-    });
-    $columnAddCard.click(function() {
-      self.addCard(new Card(prompt("Enter the name of the card")));
-    });
-
-    // construct column
-    $column.append($columnTitle)
-           .append($columnDelete)
-           .append($columnAddCard)
-           .append($columnCardList);
-            
-    // return ready column
-    return $column;
   }
-}
 
-Column.prototype = {
-  addCard: function(card) {
-    this.$element.children('ul').append(card.$element);
-  },
-  removeColumn: function() {
-    this.$element.remove();
-  }
-}
-
-function Card(description) {
-	var self = this;
+  function Column(name) {
+    var self = this;
 
     this.id = randomString();
-    this.description = description;
-    this.$element = createCard();
+    this.name = name;
+    this.$element = createColumn();
 
-    function createCard() {
-      // create components of cards
-      var $card = $('<li>').addClass('card'),
-          $cardDescription = $('<p>').addClass('card-description').text(self.description),
-          $cardDelete = $('<button>').addClass('btn-delete').text('x');
+    function createColumn() {
+      // create components of columns
+      var $column = $('<div>').addClass('column'),
+          $columnTitle = $('<h2>').addClass('column-title').text(self.name),
+          $columnCardList = $('<ul>').addClass('column-card-list'),
+          $columnDelete = $('<button>').addClass('btn-delete').text('x'),
+          $columnAddCard = $('<button>').addClass('add-card').text('Add a card');
 
-      // events      
-      $cardDelete.click(function(){
-        self.removeCard();
+      // events
+      $columnDelete.click(function() {
+        self.removeColumn();
+      });
+      $columnAddCard.click(function() {
+        self.addCard(new Card(prompt("Enter the name of the card")));
       });
 
-      // construct card
-      $card.append($cardDelete)
-           .append($cardDescription);
-
-      return $card;
+      // construct column
+      $column.append($columnTitle)
+             .append($columnDelete)
+             .append($columnAddCard)
+             .append($columnCardList);
+              
+      // return ready column
+      return $column;
     }
-}
-
-Card.prototype = {
-  removeCard: function() {
-    this.$element.remove();
   }
-}
+
+  Column.prototype = {
+    addCard: function(card) {
+      this.$element.children('ul').append(card.$element);
+    },
+    removeColumn: function() {
+      this.$element.remove();
+    }
+  }
+
+  function Card(description) {
+    var self = this;
+
+      this.id = randomString();
+      this.description = description;
+      this.$element = createCard();
+
+      function createCard() {
+        // create components of cards
+        var $card = $('<li>').addClass('card'),
+            $cardDescription = $('<p>').addClass('card-description').text(self.description),
+            $cardDelete = $('<button>').addClass('btn-delete').text('x');
+
+        // events      
+        $cardDelete.click(function(){
+          self.removeCard();
+        });
+
+        // construct card
+        $card.append($cardDelete)
+             .append($cardDescription);
+
+        return $card;
+      }
+  }
+
+  Card.prototype = {
+    removeCard: function() {
+      this.$element.remove();
+    }
+  }
+
+  var board = {
+    name: 'Kanban Board',
+    addColumn: function(column) {
+      this.$element.append(column.$element);
+      initSortable();
+    },
+    $element: $('#board .column-container')
+  };
 
 });
